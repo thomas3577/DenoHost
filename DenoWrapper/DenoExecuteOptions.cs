215cdb645186b3c772b16ed7d20d8c304f0e0444
Thomas <thomas.huber@solve.ch>
2025-06-23 21:41:30 +0200

    Cleaned up code

--- DenoWrapper/DenoExecuteOptions.cs
+++ DenoWrapper/DenoExecuteOptions.cs
@@ -10,7 +10,10 @@ public class DenoExecuteBaseOptions
 public class DenoExecuteOptions : DenoExecuteBaseOptions
 {
   public string Command { get; set; } = string.Empty;
-  public string? ConfigOrPath { get; set; }
-  public DenoConfig? Config { get; set; }
+
   public string[] Args { get; set; } = [];
+
+  public string? ConfigOrPath { get; set; }
+
+  public DenoConfig? Config { get; set; }
 }
